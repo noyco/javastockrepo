ac138af018b65cde4e7e482c2af83755ef560a76
noyco <noyco@H203LP19L.mta.ac.il>
2015-04-07 13:14:15 +0300

    math ex02

--- javastockrepo/src/com/cohen/JavastockrepoServlet.java
+++ javastockrepo/src/com/cohen/JavastockrepoServlet.java
@@ -1,13 +1,27 @@
-package com.cohen;
+package com.cohen;  
+ 
+ import java.io.IOException; 
+ import javax.servlet.http.*; 
+ 
+ 
+ @SuppressWarnings("serial") 
+ public class JavastockrepoServlet extends HttpServlet
+ { 
+ 	public void doGet(HttpServletRequest req, HttpServletResponse resp) 		
+ 			throws IOException 
+ 	{ 		
+ 		resp.setContentType("text/html");
 
-import java.io.IOException;
-import javax.servlet.http.*;
-
-@SuppressWarnings("serial")
-public class JavastockrepoServlet extends HttpServlet {
-	public void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
-	}
-}
+ 		int num1;
+ 		int num2;
+ 		int num3;
+ 		
+ 		num1 =4;
+ 		num2=3;
+ 		num3=7;
+ 		int result = ((num1+num2)*num3);
+ 		String resultStr = "<h1>Result of ("+num3+"*("+num2+"+"+num1+"))="+result+"</h1>";
+ 		
+ 		resp.getWriter().println(resultStr);
+ 	}
+} 
